5e808f9366b90bcc56dde3ef7205d6774238ea5f
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-20 15:41:57 +0100

    Show local time.

---
+++ Gu.TwinCat.TestClient/Wpf/LocalTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace Gu.TwinCat.TestClient
+{
+    using System;
+    using System.Windows.Data;
+
+    public sealed class LocalTimeConverter : IValueConverter
+    {
+        public static readonly LocalTimeConverter Default = new LocalTimeConverter();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value switch
+            {
+                DateTimeOffset x => x.LocalDateTime,
+                DateTime x => x.ToLocalTime(),
+                _ => throw new ArgumentException("Expected date time.", nameof(value))
+            };
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException($"{nameof(LocalTimeConverter)} can only be used in OneWay bindings");
+        }
+    }
+}
